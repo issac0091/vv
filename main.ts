768ce1e67b795fbd97dcf1f7251e8875dbb964de
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-01-27 19:30:15 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,22 @@
+I2C_LCD1602.LcdInit(39)
+basic.forever(function () {
+    I2C_LCD1602.ShowString("micro bit", 0, 0)
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 200)
+    }
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.Black) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 0)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 255)
+    }
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.Black)) {
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Left_Z_Motor, 255)
+        cbit_小车类.AloneCtrlSpeed(cbit_小车类.AloneState.Right_Z_Motor, 0)
+    }
+    if (cbit_小车类.Line_Sensor(cbit_小车类.enPos.LeftState, cbit_小车类.enLineState.White) && cbit_小车类.Line_Sensor(cbit_小车类.enPos.RightState, cbit_小车类.enLineState.White)) {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 200)
+    }
+})
